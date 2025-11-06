cc5231eb45436f2c7dc57394edbefa5b4b40360a
giahuytruonn <zblackz1oooo@gmail.com>
2025-11-07 00:33:17 +0700

    Complete Cancel Booking Controller

--- src/main/java/tourbooking/vietvivu/controller/BookingRequestController.java
+++ src/main/java/tourbooking/vietvivu/controller/BookingRequestController.java
@@ -1,16 +1,53 @@
 package tourbooking.vietvivu.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import tourbooking.vietvivu.dto.request.BookingRequestStatusUpdateRequest;
+import tourbooking.vietvivu.dto.response.ApiResponse;
+import tourbooking.vietvivu.dto.response.BookingRequestResponse;
+import tourbooking.vietvivu.entity.User;
+import tourbooking.vietvivu.exception.AppException;
+import tourbooking.vietvivu.exception.ErrorCode;
+import tourbooking.vietvivu.repository.UserRepository;
+import tourbooking.vietvivu.service.BookingRequestService;
 
 @RestController
 @RequestMapping("/bookings-request")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
-public class BookingRequestController {}
+public class BookingRequestController {
+
+    final BookingRequestService bookingRequestService;
+    final UserRepository userRepository;
+
+    @PutMapping("/{requestId}/status")
+    @PreAuthorize("hasRole('ADMIN')")
+    ApiResponse<BookingRequestResponse> updateStatusBooking(
+            @PathVariable String requestId, @RequestBody @Valid BookingRequestStatusUpdateRequest request) {
+
+        var context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
+        User admin =
+                userRepository.findByUsername(username).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        String adminId = admin.getId();
+
+        log.info(
+                "Updating booking request status: requestId={}, status={}, adminId={}",
+                requestId,
+                request.getStatus(),
+                adminId);
+
+        return ApiResponse.<BookingRequestResponse>builder()
+                .result(bookingRequestService.updateBookingRequestStatus(requestId, adminId, request))
+                .build();
+    }
+}
