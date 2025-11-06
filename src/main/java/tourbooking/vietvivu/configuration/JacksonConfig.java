af1e0c09d3a094ecb748cbf8a1358ceac52732af
ChucNguyen <nguyenxuanchuc111@gmail.com>
2025-11-07 01:15:29 +0700

    Create JacksonConfig và sửa tí security

---
+++ src/main/java/tourbooking/vietvivu/configuration/JacksonConfig.java
@@ -0,0 +1,28 @@
+package tourbooking.vietvivu.configuration;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class JacksonConfig {
+
+    private static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(
+                java.time.LocalDate.class,
+                new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT))
+        );
+
+        return new Jackson2ObjectMapperBuilder()
+                .modules(module)
+                .dateFormat(new java.text.SimpleDateFormat(DATE_FORMAT));
+    }
+}
