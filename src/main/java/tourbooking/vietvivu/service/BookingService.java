4ad5789dd5b846b46b0727b499335ba12b6f574f
ChucNguyen <nguyenxuanchuc111@gmail.com>
2025-12-08 22:32:02 +0700

    Xong quan ly user

--- src/main/java/tourbooking/vietvivu/service/BookingService.java
+++ src/main/java/tourbooking/vietvivu/service/BookingService.java
@@ -286,6 +286,7 @@ public class BookingService {
 
         List<Booking> bookings = bookingRepository.findByUser(user);
 
+
         return bookings.stream().map(this::mapToBookingResponse).collect(Collectors.toList());
     }
 }
